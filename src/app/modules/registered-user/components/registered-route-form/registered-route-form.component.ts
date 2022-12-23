1c16198da6f8139aab68e11ea4ca5f153bdbc8fb
unknown <ognjen34car@gmail.com>
2022-12-23 19:27:27 +0100

    feature: implemented driver selection on map

--- src/app/modules/registered-user/components/registered-route-form/registered-route-form.component.ts
+++ src/app/modules/registered-user/components/registered-route-form/registered-route-form.component.ts
@@ -15,7 +15,10 @@ import {MapService} from "../../../../components/map/map.service";
   styleUrls: ['./registered-route-form.component.css']
 })
 export class RegisteredRouteFormComponent implements OnInit {
-
+  @ViewChild('popupContent') popupContent?: ElementRef;
+  @ViewChild('driverName') driverName?: ElementRef;
+  @ViewChild('driverPhone') driverPhone?: ElementRef;
+  @ViewChild('driverEmail') driverEmail?: ElementRef;
 
 
     async go(id: string) {
@@ -31,38 +34,73 @@ export class RegisteredRouteFormComponent implements OnInit {
           document.getElementById("moreOptions").style.display = "block"
           document.getElementById("test")!.style.top = "60%"
 
-         this.driverService.getAll().subscribe(value =>
-         {
 
-           for(let driver of value.results)
-           {
-
-               this.driverService.setLocation(driver);
-
-
-           }
-         })
-
-          /*for(let driver of this.drivers?.results!)
-          {
-            console.log(driver);
-
-          }*/
 
         }
+      }
+      if (id == "order-button")
+      {
+        this.routeFormService.RemoveAllMarkers();
+        this.driverService.getAll().subscribe(value =>
+        {
+
+          for(let driver of value.results)
+          {
+            this.driverService.getDriverVehicle(driver.id).subscribe(vehicle =>
+             {
+              if(this.goForm.get("carType")?.value == "" || this.goForm.get("carType")?.value== vehicle.vehicleType) {
+                if (!this.goForm.get("forBabies")?.value && !this.goForm.get("forPets")?.value)
+                  this.driverService.setLocation(driver);
+
+                if (this.goForm.get("forBabies")?.value) {
+                  if (vehicle.babyTransport)
+                    if (this.goForm.get("forPets")?.value)
+                      if (vehicle.petTransport)
+                        this.driverService.setLocation(driver);
+                } else if (this.goForm.get("forPets")?.value)
+                  if (vehicle.petTransport)
+                    if (this.goForm.get("forBabies")?.value)
+                      if (vehicle.babyTransport)
+                        this.driverService.setLocation(driver);
+              }
+
+
+            })
+
+
+          }
+        })
+      }
+      if (id == "cancel-button")
+      {
+        this.routeFormService.RemoveAllMarkers();
       }
 
 
     }
   goForm = new FormGroup({
     location: new FormControl("",[Validators.required]),
-    destination: new FormControl("",[Validators.required])
+    destination: new FormControl("",[Validators.required]),
+    carType: new FormControl(""),
+    forBabies: new FormControl(),
+    forPets: new FormControl()
   });
 
   constructor(private routeFormService:RouteFormService ,private driverService:DriverService ,private mapService:MapService) { }
 
   ngOnInit(): void {
-    this.mapService.selectLocation$.subscribe({next:(driver)=>{
+    this.mapService.selectLocation$.subscribe({next:(driver:DriverInfoDTO)=>{
         console.log(driver)
+        // @ts-ignore
+        this.popupContent.nativeElement.style.display = 'block';
+
+        // @ts-ignore
+        this.driverName.nativeElement.textContent = driver.name + " " + driver.surname;
+        // @ts-ignore
+        this.driverPhone.nativeElement.textContent = driver.telephoneNumber;
+        // @ts-ignore
+        this.driverEmail.nativeElement.textContent = driver.email;
+
+
       } })
   }}
