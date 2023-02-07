09523fc02a60bd3d8e648f996653830d63d1322c
Dušan Rožić <96205750+rozicd@users.noreply.github.com>
2023-02-07 21:36:57 +0100

    feat: added login component test and register  test wip

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,23 +1,76 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import {AuthService} from "../../_service/auth.service";
+import {MatDialog, MatDialogModule} from "@angular/material/dialog";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterTestingModule} from "@angular/router/testing";
+import {of, throwError} from "rxjs";
+import {RideNotificationComponent} from "../dialogs/ride-notification/ride-notification.component";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let authService: AuthService;
+  let dialog: MatDialog
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        MatDialogModule
+      ],
+      declarations: [LoginComponent],
+      providers: [AuthService]
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    authService = TestBed.get(AuthService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+
+  it('form should be invalid when empty', () => {
+    expect(component.loginForm.valid).toBeFalsy();
+  });
+
+  it('username field validity', () => {
+    let username = component.loginForm.controls['username'];
+    expect(username.valid).toBeFalsy();
+
+    username.setValue("");
+    expect(username.hasError('required')).toBeTruthy();
+
+    username.setValue("test");
+    expect(username.valid).toBeTruthy();
+  });
+
+  it('password field validity', () => {
+    let password = component.loginForm.controls['password'];
+    expect(password.valid).toBeFalsy();
+
+    password.setValue("");
+    expect(password.hasError('required')).toBeTruthy();
+
+    password.setValue("test");
+    expect(password.valid).toBeTruthy();
+  });
+
+  it('form should be valid when all fields are filled', () => {
+    component.loginForm.controls['username'].setValue("test");
+    component.loginForm.controls['password'].setValue("test");
+    expect(component.loginForm.valid).toBeTruthy();
+  });
+
+
+}
+)
--- src/app/components/register/register.component.spec.ts
+++ src/app/components/register/register.component.spec.ts
@@ -1,23 +1,119 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule, MatDialog } from '@angular/material/dialog';
+import { of } from 'rxjs';
 
 import { RegisterComponent } from './register.component';
+import {AuthService} from "../../_service/auth.service";
+import {RideNotificationComponent} from "../dialogs/ride-notification/ride-notification.component";
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
-  let fixture: ComponentFixture<RegisterComponent>;
+  let authService: AuthService;
+  let dialog: MatDialog;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+        ReactiveFormsModule,
+        MatDialogModule
+      ],
+      declarations: [
+        RegisterComponent,
+        RideNotificationComponent
+      ],
+      providers: [
+        RegisterComponent,
+        AuthService,
+        MatDialog
+      ]
     })
-    .compileComponents();
+      .compileComponents();
+  }));
 
-    fixture = TestBed.createComponent(RegisterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    component = TestBed.inject(RegisterComponent);
+    authService = TestBed.inject(AuthService);
+    dialog = TestBed.inject(MatDialog);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+
+  it('form invalid when empty', () => {
+    expect(component.registerForm.valid).toBeFalsy();
+  });
+
+  it('first name field validity', () => {
+    let errors = {};
+    let firstName = component.registerForm.controls['firstName'];
+    expect(firstName.valid).toBeFalsy();
+
+    // First name field is required
+    errors = firstName.errors || {};
+    expect(errors['required']).toBeTruthy();
+
+    // Set first name to something invalid
+    firstName.setValue("");
+    errors = firstName.errors || {};
+    expect(errors['required']).toBeTruthy();
+
+    // Set first name to something valid
+    firstName.setValue("John");
+    errors = firstName.errors || {};
+    expect(errors['required']).toBeFalsy();
+
+    // First name must start with an uppercase letter
+    firstName.setValue("john");
+    errors = firstName.errors || {};
+    expect(errors['pattern']).toBeTruthy();
+  });
+  it('last name field validity', () => {
+    let errors = {};
+    let lastName = component.registerForm.controls['lastName'];
+    expect(lastName.valid).toBeFalsy();
+
+    // First name field is required
+    errors = lastName.errors || {};
+    expect(errors['required']).toBeTruthy();
+
+    // Set first name to something invalid
+    lastName.setValue("");
+    errors = lastName.errors || {};
+    expect(errors['required']).toBeTruthy();
+
+    // Set first name to something valid
+    lastName.setValue("John");
+    errors = lastName.errors || {};
+    expect(errors['required']).toBeFalsy();
+
+    // First name must start with an uppercase letter
+    lastName.setValue("john");
+    errors = lastName.errors || {};
+    expect(errors['pattern']).toBeTruthy();
+  });
+  it('password field validity', () => {
+    let errors = {};
+    let password = component.registerForm.controls['password'];
+    expect(password.valid).toBeFalsy();
+
+
+    errors = password.errors || {};
+    expect(errors['required']).toBeTruthy();
+
+
+    password.setValue("");
+    errors = password.errors || {};
+    expect(errors['required']).toBeTruthy();
+
+
+    password.setValue("john");
+    errors = password.errors!;
+    expect(errors['minLength']).toBeTruthy();
+  });
+})
