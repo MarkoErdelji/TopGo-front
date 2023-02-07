b07c2de51f56a1aeea6929e0ae339bacfffd160c
MarkoErdelji <erdeljimarko@gmail.com>
2023-02-07 19:46:34 +0100

    fix: fixed merge errors

--- src/app/_service/auth.service.spec.ts
+++ src/app/_service/auth.service.spec.ts
@@ -8,51 +8,43 @@ import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { JwtHelperService } from '@auth0/angular-jwt';
-import { JWTTokenDTO } from '../modules/DTO/JWTTokenDTO';
 
 describe('AuthService', () => {
   let service: AuthService;
-  let httpController: HttpTestingController;
-  let router: Router;
-  let dialog: MatDialog;
-
-  let url = 'localhost:8080/api/user';
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [
-        AuthService,
-        Router,
-        MatDialog,
-        JwtHelperService,
-      ],
+      providers: [AuthService],
     });
+
     service = TestBed.inject(AuthService);
-    httpController = TestBed.inject(HttpTestingController);
-    router = TestBed.inject(Router);
-    dialog = TestBed.inject(MatDialog);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   afterEach(() => {
-    httpController.verify();
+    httpTestingController.verify();
   });
 
-  describe('signIn', () => {
-    it('should return a token upon successful login', () => {
-      const email = 'test@example.com';
-      const password = 'password';
-      let jwtDTO: JWTTokenDTO = {accessToken: '123456', refreshToken: '67890'};
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
 
-      service.signIn(email, password).subscribe((res) => {
-        expect(res).toEqual(jwtDTO);
-      });
+  it('should sign in the user', () => {
+    const username = 'test@test.com';
+    const password = 'testpassword';
+    const expectedResponse:any= {
+      accessToken: 'access_token_123',
+      refreshToken: 'access_token_123'
+    };
 
-      let tokenResponse = httpController.expectOne({
-        method: 'POST',
-        url: `${url}/login`,
-      });
-      tokenResponse.flush(jwtDTO);
+    service.signIn(username, password).subscribe((response) => {
+      expect(response).toEqual(expectedResponse);
     });
+
+    const req = httpTestingController.expectOne(`${service.endpoint}/user/login`);
+    expect(req.request.method).toEqual('POST');
+    req.flush(expectedResponse);
   });
 });
